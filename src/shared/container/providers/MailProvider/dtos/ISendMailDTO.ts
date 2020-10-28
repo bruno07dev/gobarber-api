a618f36ce49afb01751f4168a67e98ad9fa0c707
Bruno Cardoso <brunomusic_3@hotmail.com>
2020-10-28 16:45:26 -0300

    configuring the service provider

---
+++ src/shared/container/providers/MailProvider/dtos/ISendMailDTO.ts
@@ -0,0 +1,13 @@
+import IParseMailDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
+
+interface IMailContent {
+  name: string;
+  email: string;
+}
+
+export default interface ISendMailDTO {
+  to: IMailContent;
+  from?: IMailContent;
+  subject: string;
+  templateData: IParseMailDTO;
+}
